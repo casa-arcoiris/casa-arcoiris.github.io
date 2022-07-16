e55e072878e0b477790bd31296c47e815d9de221
casa-arcoiris <casa_arcoiris@outlook.com>
2022-07-16 12:57:41 -0500

    Estrés

---
+++ publicaciones/jovenes/el_estres.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3B6TNMJMS1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-3B6TNMJMS1');
+    </script>
+    
+	<meta charset="UTF-8">
+    <meta name="description" content="Todas las personas nos hemos sentido o nos sentiremos estresadas, pero ¿Qué podemos hacer para disminuirlo? ¡Aquí te decimos!">
+    <meta name="keywords" content="casa, arcoíris, comunidad, lgbtq, casa arcoíris, comunidad lgbtq, estrés, psicología, crecimiento, pediatría, adolescentes, jóvenes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>El estrés | Casa Arcoíris</title>
+	<link rel="stylesheet" href="https://www.casa-arcoiris.org/css/style.css">
+	<script src="https://www.casa-arcoiris.org/js/main.js"></script>
+    <link rel="shortcut icon" href="https://www.casa-arcoiris.org/assets/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://www.casa-arcoiris.org/assets/favicon.png">
+</head>
+<body class="jovenes_post-bg">
+
+	<header>
+		<div class="header_neon" id="header_neon"></div>
+		<a class="header_link_logo" href="https://www.casa-arcoiris.org"><img src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" class="header_logo" alt="Logo de Casa Arcoíris"></a>
+		<svg tabindex="0" onclick="menu_f()" id="menu_icon" class="menu_icon" viewBox="0 0 100 80" width="40" height="40">
+  			<rect y="0" width="100" height="20" rx="8"></rect>
+  			<rect y="30" width="100" height="20" rx="8"></rect>
+  			<rect y="60" width="100" height="20" rx="8"></rect>
+		</svg>
+		<ul id="menu" class="menu_list">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+            <button class="switch" id="switch">
+                <span>
+
+                    <i class="sun">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+                        </svg>
+                    </i>
+
+                </span>
+                <span>
+
+                    <i class="moon">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+                        </svg>
+                    </i>
+
+                </span>
+            </button>
+		</ul>
+		<ul id="menu_list-no_mobile" class="menu_list-no_mobile">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+            <button class="switch" id="switch-no_mobile">
+                <span>
+
+                    <i class="sun">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+                        </svg>
+                    </i>
+
+                </span>
+                <span>
+
+                    <i class="moon">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+                        </svg>
+                    </i>
+
+                </span>
+            </button>
+		</ul>
+	</header>
+
+	<div class="space"></div>
+
+	<div itemscope itemtype="http://schema.org/Article" class="post-bg">
+
+		<div class="post_text">
+			<h1 itemprop="name">El estrés</h1>
+
+			<p>Todas las personas nos hemos sentido o nos sentiremos estresadas, pero ¿Qué podemos hacer para disminuirlo? ¡Aquí te decimos!</p>
+
+			<img itemprop="image" class="post_image" src="https://www.casa-arcoiris.org/assets/estres.jpg" alt="Imagen representando a una persona estresada">
+			<p class="post_image-small">Imagen representando a una persona estresada</p>
+
+            <span itemprop="articleBody">
+    			<h2>¿Cómo se manifiesta el estrés en el cuerpo?</h2>
+
+    			<p>El estrés se puede manifestar de muchas maneras en el cuerpo. Algunas maneras son:</p>
+
+                <ul class="list_publicacion">
+                    <li><p>Caída del cabello</p></li>
+                    <li><p>Ansiedad</p></li>
+                    <li><p>Pérdida del apetito</p></li>
+                    <li><p>Aumento del apetito</p></li>
+                    <li><p>Problemas en la piel como acné, dermatitis o psoriasis</p></li>
+                    <li><p>Problemas menstruales</p></li>
+                    <li><p>Dolores de cabeza</p></li>
+                    <li><p>Problemas estomacales (acidez, flatulencias, diarrea, estreñimiento, etc)</p></li>
+                    <li><p>Tensiones musculares</p></li>
+                    <li><p>Dificultad para respirar</p></li>
+                </ul>
+
+                <h2>¿El estrés afecta al crecimiento?</h2>
+
+                <p>Cuando es un estrés  <a onclick="goToSection('estres_agudo')" onkeydown="goToSection('estres_agudo')" class="same_page_link jovenes_link" tabindex="0">agudo</a>, no afecta al crecimiento. Sin embargo, si es un estrés  <a onclick="goToSection('estres_cronico')" onkeydown="goToSection('estres_cronico')" class="same_page_link jovenes_link" tabindex="0">crónico</a> que se ha tenido por un largo plazo de tiempo, sí puede afectarlo.</p>
+
+                <h2 id="estres_agudo">¿Cuál es el estrés agudo?</h2>
+
+                <p>El estrés agudo es el estrés más común. Surge de las exigencias y presiones del pasado reciente y del futuro cercano. Se siente agradable cuando es en pequeñas dosis, pero cuando es demasiado se siente agotador. Por ejemplo, cuando despiertas y sientes una emoción placentera de lo que viene en el día, es estrés en una pequeña dosis, pero cuando regresas de un día muy pesado, sientes agotamiento.</p>
+
+                <h2 id="estres_cronico">¿Cuál es el estrés crónico?</h2>
+
+                <p>A diferencia del estrés agudo, el estrés crónico no es agradable. El estrés crónico es el estrés agotador que desgasta a las personas día a día. El estrés crónico surge cuando una persona nunca ve la salida de una situación deprimente o desgastante. Las personas que tienen estrés crónico olvidan que lo tienen y se acostumbran a tenerlo. El estrés crónico puede causar efectos devastadores en la salud. Los síntomas del estrés crónico son difíciles de tratar y pueden requerir tratamiento médico y psicológico.</p>
+
+                <h2>¿De qué me puedo enfermar si me estreso demasiado?</h2>
+
+                <p>El estrés no causa enfermedades, sin embargo, si se está viviendo con estrés crónico, se pueden bajar las defensas del cuerpo y aumentar el riesgo de atrapar alguna enfermedad.</p>
+
+                <h2>¿Cómo afecta el estrés psicológicamente a largo plazo?</h2>
+
+                <p>El estrés se puede reflejar tanto físicamente como psicológicamente. Algunos síntomas psicológicos del estrés pueden ser:</p>
+
+                <ul class="list_publicacion">
+                    <li><p>Fatiga</p></li>
+                    <li><p>Cambios de ánimo</p></li>
+                    <li><p>Falta de concentración</p></li>
+                    <li><p>Insomnio</p></li>
+                    <li><p>Irritabilidad</p></li>
+                </ul>
+
+                <h2>¿De qué manera puede afectar el estrés en mis relaciones?</h2>
+
+                <p>El estrés puede hacer que te dejes de comunicar de una manera clara, empática y directa, lo cual puede afectar a tus relaciones sociales.</p>
+
+                <p>Es muy importante tener presente que, aunque no la estés pasando bien, tienes que seguir teniendo cuidado con la forma en la que te comunicas con las personas que tienes a tu alrededor.</p>
+
+                <h2>¿El estrés es malo psicológicamente?</h2>
+
+                <p>El estrés, como el resto de nuestras emociones, es adaptable. Aunque no todo lo que sentimos es placentero o agradable, nos sirve como "alerta" sobre nuestra realidad; siempre que una sensación o emoción nos afecta de una manera poco agradable, nos está dando información acerca de situaciones sobre las cuales podemos y debemos actuar.</p>
+
+                <h2>¿De qué manera me puede ayudar el estrés?</h2>
+
+                <p>El estrés nos ayuda a reaccionar ante riesgos y peligros como una "señal de alarma", provoca acciones rápidas cuando necesitamos producir respuestas físicas e intelectuales de emergencia, nos ayuda a generar resistencia gracias a actividades deportivas, entre otros beneficios.</p>
+
+                <h2>¿El estrés tiene alguna relación con el miedo?</h2>
+
+                <p>El estrés y el miedo sí están vinculados, ya que el estrés es una respuesta del cuerpo y mente ante situaciones que de alguna manera nos representan algún peligro, y el miedo aparece cuando creemos que vamos a sufrir daño.</p>
+
+                <p>Te invitamos a leer nuestra publicación <a target="_blank" href="https://www.casa-arcoiris.org/publicaciones/jovenes/emocionario.html" class="same_page_link jovenes_link">Emocionario</a> para obtener más información acerca de las emociones.</p>
+
+                <h2>¿Se puede vivir sin estresarse nunca?</h2>
+
+                <p>No, no se puede. Toda la vida convivimos con emociones que nos son placenteras y con otras que no lo son, es necesario aprender a convivir con estas sensaciones, incluso cuando no nos son agradables.</p>
+
+                <h2>¿Qué actividades ayudan a disminuir el estrés?</h2>
+
+                <p>Aprender a controlar el estrés es un ejercicio constante. Puedes hacer distintas actividades físicas y psicológicas para apoyar los procesos del estrés. Por ejemplo:</p>
+
+                <ul class="list_publicacion">
+                    <li><p>Hacer actidades deportivas que sean de agrado personal.</p></li>
+                    <li><p>Aprender y practicar ejercicios de respiración.</p></li>
+                </ul>
+
+                <h2>¿Qué es recomendable para vivir con el menor estrés posible?</h2>
+
+                <ul class="list_publicacion">
+                    <li><p>Hacer actividades que sean de agrado personal</p></li>
+                    <li><p>Hacer ejercicio</p></li>
+                    <li><p>Dormir temprano</p></li>
+                    <li><p>Comer bien</p></li>
+                    <li><p>Tener un estilo de vida saludable</p></li>
+                    <li><p>Crear un diario donde le puedas dar seguimiento a tus emociones.</p></li>
+                    <li><p>Aprender a identificar y mantenerte al tanto de cuáles son las situaciones que te generan estrés.</p></li>
+                </ul>
+
+			</span>
+
+            <div class="referencias jovenes_link">
+                <h3>Referencias: </h3>
+
+                <a target="_blank" href="https://www.apa.org/topics/stress/tipos">https://www.apa.org/topics/stress/tipos</a>
+            </div>
+
+            <h2>Agradecimientos especiales:</h2>
+
+            <p>Le agradecemos al equipo psicológico y pediátrico de "Casa Arcoíris" por ayudarnos respondiendo las preguntas de esta publicación.</p>
+
+			<h3 class="categorias_h3">Categoría</h3>
+			<div class="categorias_post">
+				<div class="categoria_post jovenes">Jóvenes</div>
+			</div>
+		</div>
+
+		
+		<div class="published">
+			<svg class="clock_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>
+			<p>Publicado el <span itemprop="datePublished" content="2022-07-??">??/07/2022</span></p>
+		</div>
+
+	</div>
+
+    <footer>
+        <div class="footer_content">
+            <p class="copyright footer_text"></p>
+            <a href="https://www.casa-arcoiris.org"><img class="footer_logo" src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" alt="Logo de Casa Arcoíris"></a>
+        </div>
+    </footer>
+
+    <div id="close_element"></div>
+
+	<script src="https://www.casa-arcoiris.org/js/second.js"></script>
+    <script src="https://www.casa-arcoiris.org/js/dark-mode.js"></script>
+</body>
+</html>
