6bb1d3f64b179a668ab0fc80427a17f764f27a8e
casa-arcoiris <casa_arcoiris@outlook.com>
2022-07-23 10:45:25 -0500

    Mails

--- mails/unsubscribe.php
+++ mails/unsubscribe.php
@@ -17,5 +17,6 @@ $headers = 'MIME-Version 1.0'."\r\n";
 $headers .= "From:" . $from . "\r\n";
 $headers .= "Content-type: text/html; charset-iso-8859-1\r\n";
 $headers .= "X-Mailer: PHP/".phpversion();
-mail($to,$subject,$message, $headers, "-f$from");
+$headers .= 'List-Unsubscribe: <https://www.casa-arcoiris.org/desuscribir.php?email='.$email.'>"\r\n"';
+mail($to, $subject, $message, $headers, "-f$from");
 ?>
