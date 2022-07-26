92ed303dbc14325e28505c6b15636e35583f3425
casa-arcoiris <casa_arcoiris@outlook.com>
2022-07-25 21:23:01 -0500

    Pansexualidad

---
+++ publicaciones/lgbtq/pansexualidad.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-3B6TNMJMS1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-3B6TNMJMS1');
+	</script>
+	
+	<meta charset="UTF-8">
+	<meta name="description" content="Seguramente has escuchado la palabra 'pansexual', pero ¿Qué significa? ¿Es lo mismo que la bisexualidad? ¿Es una confusión?">
+	<meta name="keywords" content="casa, arcoíris, comunidad, lgbtq, nuestro, objetivo, inicio, casa arcoíris, nuestro objetivo, comunidad lgbtq, pansexual, pansexualidad, bandera pansexual">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pansexualidad | Casa Arcoíris</title>
+	<link rel="stylesheet" href="https://www.casa-arcoiris.org/css/style.css">
+	<script src="https://www.casa-arcoiris.org/js/main.js"></script>
+	<link rel="shortcut icon" href="https://www.casa-arcoiris.org/assets/favicon.png" type="image/x-icon">
+	<link rel="apple-touch-icon" href="https://www.casa-arcoiris.org/assets/favicon.png">
+</head>
+<body>
+	<div class="pansexual_post-bg" id="post_background"></div>
+
+	<header>
+		<div class="header_pansexual" id="header_moving_right"></div>
+		<a class="header_link_logo" href="https://www.casa-arcoiris.org"><img src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" class="header_logo" alt="Logo de Casa Arcoíris"></a>
+		<svg tabindex="0" onclick="menu_f()" id="menu_icon" class="menu_icon" viewBox="0 0 100 80" width="40" height="40">
+  			<rect y="0" width="100" height="20" rx="8"></rect>
+  			<rect y="30" width="100" height="20" rx="8"></rect>
+  			<rect y="60" width="100" height="20" rx="8"></rect>
+		</svg>
+		<ul id="menu" class="menu_list">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+			<button class="switch" id="switch">
+				<span>
+
+					<i class="sun">
+						<svg viewBox="0 0 512 512">
+							<path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+						</svg>
+					</i>
+
+				</span>
+				<span>
+
+					<i class="moon">
+						<svg viewBox="0 0 512 512">
+							<path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+						</svg>
+					</i>
+
+				</span>
+			</button>
+		</ul>
+		<ul id="menu_list-no_mobile" class="menu_list-no_mobile">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+			<button class="switch" id="switch-no_mobile">
+				<span>
+
+					<i class="sun">
+						<svg viewBox="0 0 512 512">
+							<path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+						</svg>
+					</i>
+
+				</span>
+				<span>
+
+					<i class="moon">
+						<svg viewBox="0 0 512 512">
+							<path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+						</svg>
+					</i>
+
+				</span>
+			</button>
+		</ul>
+	</header>
+
+	<div class="space"></div>
+
+	<div itemscope itemtype="http://schema.org/Article" class="post-bg">
+		
+        <a href="https://www.casa-arcoiris.org/publicaciones.html" class="back-to-posts"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg> <p>Volver a las publicaciones</p></a>
+
+		<div class="post_text">
+			<h1 itemprop="name">Pansexualidad</h1>
+
+			<p>Seguramente has escuchado la palabra "pansexual", pero ¿Qué significa? ¿Es lo mismo que la bisexualidad? ¿Es una confusión?</p>
+
+			<img itemprop="image" class="post_image" src="https://www.casa-arcoiris.org/assets/pansexual_flag.webp" alt="Bandera pansexual">
+			<p class="post_image-small">Bandera pansexual</p>
+
+			<span itemprop="articleBody">
+
+				<h2>¿Qué es la pansexualidad?</h2>
+
+				<p>La pansexualidad es una orientación sexual. A las personas pansexuales les atraen las cualidades de una persona (su manera de pensar, personalidad, humor, valores, belleza, etc), sin importar su género o su orientación sexual.</p>
+
+				<h2>¿A las personas pansexuales les importa si soy trans?</h2>
+
+				<p>A las personas pansexuales no les interesa si alguien se identifica como trans, cisgénero, no binario, etc.</p>
+
+				<h2>¿La pansexualidad y la bisexualidad son iguales?</h2>
+
+				<p>No. La bisexualidad es la atracción romántica y/o sexual hacia hombres, mujeres, personas transgénero y no binarias.</p>
+
+				<p>La pansexualidad es la atracción romántica y/o sexual sin importar su género o su orientación sexual.</p>
+
+				<p>Te invitamos a leer nuestra publicación <a target="_blank" href="https://www.casa-arcoiris.org/publicaciones/lgbtq/bisexualidad.php" class="same_page_link">Bisexualidad</a> para obtener más información acerca de las personas bisexuales.</p>
+
+				<h2>¿Las personas pansexuales están confundidas?</h2>
+
+				<p>No. Las personas que salen del clóset lo hacen porque la confusión acabó, no porque haya empezado.</p>
+
+				<h2>¿A las personas pansexuales les atrae todo el mundo?</h2>
+
+				<p>No. Las personas pansexuales tienen sus gustos, estándares y preferencias al igual que una persona heterosexual u homosexual.</p>
+
+				<h2>¿Las personas pansexuales tienen sexo todo el tiempo?</h2>
+
+				<p>El sentirse atracción romántica y/o sexual hacia una persona es independiente a tener una vida sexual activa. Las personas pansexuales pueden sentirse atraídas hacia otras personas y no tener sexo con ellas.</p>
+
+				<h2>¿Las personas pansexuales son promiscuas?</h2>
+
+				<p>La pansexualidad y ser una persona promiscua son independientes una de la otra. La orientación sexual no tiene nada que ver con la promiscuidad.</p>
+
+				<h2>¿Las personas pansexuales son infieles?</h2>
+
+				<p>No. La pansexualidad no está de la mano con ser una persona infiel. La infidelidad es independiente de la orientación sexual.</p>
+
+				<h2>¿Ser pansexual es una moda?</h2>
+
+				<p>No. El término ha ganado popularidad porque muchas personas se han identificado con él, pero no es algo nuevo. La pansexualidad siempre ha existido. Las personas que se declaran pansexuales no lo hacen por seguir alguna moda, es por encontrar un término que las represente.</p>
+
+			</span>
+
+			<div class="referencias">
+				<h3>Referencias: </h3>
+
+				<a href="https://www.plannedparenthood.org/es/blog/que-es-ser-pansexual" target="_blank">https://www.plannedparenthood.org/es/blog/que-es-ser-pansexual</a>
+				<a href="https://www.youthkiawaaz.com/2015/09/frequently-asked-questions-pansexual/" target="_blank">https://www.youthkiawaaz.com/2015/09/frequently-asked-questions-pansexual/</a>
+				<a href="https://www.shock.co/cultura-pop/5-preguntas-sobre-la-pansexualidad-y-sus-respuestas" target="_blank">https://www.shock.co/cultura-pop/5-preguntas-sobre-la-pansexualidad-y-sus-respuestas</a>
+			</div>
+			
+			<h3 class="categorias_h3">Categorías</h3>
+			<div class="categorias_post">
+				<div class="categoria_post lgbtq">LGBTQ+</div>
+				<div class="categoria_post pansexual">Pansexual</div>
+			</div>
+		</div>
+
+		
+		<div class="published">
+			<svg class="clock_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>
+			<p>Publicado el <span itemprop="datePublished" content="2022-08-??">??/08/2022</span></p>
+		</div>
+
+	</div>
+
+	<footer>
+		<div class="footer_content">
+			<p class="copyright footer_text"></p>
+			<a href="https://www.casa-arcoiris.org"><img class="footer_logo" src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" alt="Logo de Casa Arcoíris"></a>
+		</div>
+	</footer>
+
+	<div id="close_element"></div>
+
+	<script src="https://www.casa-arcoiris.org/js/second.js"></script>
+	<script src="https://www.casa-arcoiris.org/js/dark-mode.js"></script>
+</body>
+</html>
