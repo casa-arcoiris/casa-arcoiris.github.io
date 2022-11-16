eaec338c4a75f6f7f5cedd698b34bda5742d1cdb
casa-arcoiris <casa_arcoiris@outlook.com>
2022-11-15 20:30:41 -0600

    Ansiedad

---
+++ publicaciones/jovenes/la_ansiedad.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3B6TNMJMS1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-3B6TNMJMS1');
+    </script>
+    
+	<meta charset="UTF-8">
+    <meta name="description" content="¿Alguna vez has experimentado ansiedad? ¿Sabes lo que es? ¿Sabes cómo se siente? ¡Aquí te decimos todo lo que necesitas saber!">
+    <meta name="keywords" content="casa, arcoíris, comunidad, lgbtq, casa arcoíris, comunidad lgbtq, ansiedad, psicología, adolescentes, jóvenes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>La ansiedad | Casa Arcoíris</title>
+	<link rel="stylesheet" href="https://www.casa-arcoiris.org/css/style.css">
+	<script src="https://www.casa-arcoiris.org/js/main.js"></script>
+    <link rel="shortcut icon" href="https://www.casa-arcoiris.org/assets/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://www.casa-arcoiris.org/assets/favicon.png">
+</head>
+<body class="jovenes_post-bg">
+
+	<header>
+		<div class="header_neon" id="header_neon"></div>
+		<a class="header_link_logo" href="https://www.casa-arcoiris.org"><img src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" class="header_logo" alt="Logo de Casa Arcoíris"></a>
+		<svg tabindex="0" onclick="menu_f()" id="menu_icon" class="menu_icon" viewBox="0 0 100 80" width="40" height="40">
+  			<rect y="0" width="100" height="20" rx="8"></rect>
+  			<rect y="30" width="100" height="20" rx="8"></rect>
+  			<rect y="60" width="100" height="20" rx="8"></rect>
+		</svg>
+		<ul id="menu" class="menu_list">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+            <button class="switch" id="switch">
+                <span>
+
+                    <i class="sun">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+                        </svg>
+                    </i>
+
+                </span>
+                <span>
+
+                    <i class="moon">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+                        </svg>
+                    </i>
+
+                </span>
+            </button>
+		</ul>
+		<ul id="menu_list-no_mobile" class="menu_list-no_mobile">
+			<li><a href="https://www.casa-arcoiris.org">Inicio</a></li>
+			<li><a href="https://www.casa-arcoiris.org/publicaciones.html">Publicaciones</a></li>
+            <button class="switch" id="switch-no_mobile">
+                <span>
+
+                    <i class="sun">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z"></path>
+                        </svg>
+                    </i>
+
+                </span>
+                <span>
+
+                    <i class="moon">
+                        <svg viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" class=""></path>
+                        </svg>
+                    </i>
+
+                </span>
+            </button>
+		</ul>
+	</header>
+
+	<div class="space"></div>
+
+	<div itemscope itemtype="http://schema.org/Article" class="post-bg">
+
+        <a href="https://www.casa-arcoiris.org/publicaciones.html" class="back-to-posts"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/></svg> <p>Volver a las publicaciones</p></a>
+
+		<div class="post_text">
+			<h1 itemprop="name">La ansiedad</h1>
+
+			<p>¿Alguna vez has experimentado ansiedad? ¿Sabes lo que es? ¿Sabes cómo se siente? ¡Aquí te decimos todo lo que necesitas saber!</p>
+
+			<img itemprop="image" class="post_image" src="https://www.casa-arcoiris.org/assets/ansiedad.jpg" alt="Imagen representando a una persona con ansiedad">
+			<p class="post_image-small">Imagen representando a una persona con ansiedad</p>
+
+            <span itemprop="articleBody">
+                <h2>¿Qué es la ansiedad?</h2>
+
+                <p>La ansiedad es un sentimiento de miedo, temor e inquietud que se presenta cuando una persona se enfrenta o se va a entrentar a una situación que le parece amenzante.</p>
+
+    			<h2>¿Cómo se manifiesta la ansiedad?</h2>
+
+    			<p>La ansiedad se puede manifestar de muchas maneras. Algunas maneras son:</p>
+
+                <ul class="list_publicacion">
+                    <li><p>Ritmo cardiaco más rápido</p></li>
+                    <li><p>Respiración más rápida</p></li>
+                    <li><p>Sensación de intranquilidad o temor</p></li>
+                    <li><p>Sudoración</p></li>
+                    <li><p>Problemas estomacales (acidez, flatulencias, diarrea, estreñimiento, etc)</p></li>
+                    <li><p>Tensiones musculares</p></li>
+                    <li><p>Dolor en el pecho</p></li>
+                    <li><p>Dificultad para respirar</p></li>
+                    <li><p>Problemas con el sueño (dormir mucho o dormir poco)</p></li>
+                    <li><p>Problemas de concentración y pensamientos constantes de la preocupación que se tiene</p></li>
+                </ul>
+
+                <h2>¿La ansiedad es una consecuencia del estrés?</h2>
+
+                <p>Hay veces en las que el estrés se puede convertir en ansiedad, sin embargo, no siempre es así.</p>
+
+                <h2>¿Cómo diferenciar entre el estrés y la ansiedad?</h2>
+
+                <p>El estrés y la ansiedad tienen síntomas muy similares, por lo que puede ser difícil diferenciarlos</p>
+
+                <p>Una característica de ambas es la duración, ya que el estrés suele durar poco tiempo, mientras que la ansiedad puede permanecer.</p>
+
+                <p>Te invitamos a leer nuestra publicación <a target="_blank" href="https://www.casa-arcoiris.org/publicaciones/jovenes/el_estres.php" class="same_page_link jovenes_link">El estrés</a> para obtener más información acerca del estrés, sus causas y sus síntomas.</p>
+
+                <h2>¿La ansiedad es mala psicológicamente?</h2>
+
+                <p>Aunque la ansiedad puede resultar poco placentera en cantidades grandes, tiene sus beneficios en dosis pequeñas. Además de activar un estado de alerta, puede ayudar en situaciones en las que no se quiere fallar y le ayuda a las personas a dar lo mejor de sí mismas.</p>
+
+                <h2>¿De qué manera me puede ayudar la ansiedad?</h2>
+
+                <p>La ansiedad puede ayudar a las personas a dar lo mejor de sí, ayuda a tomar mejores decisiones, agudiza los sentidos, entre otras cosas.</p>
+
+                <h2>¿La ansiedad tiene alguna relación con el miedo?</h2>
+
+                <p>La ansiedad y el miedo se pueden sentir de una manera similar. La diferencia es que es producen ante situaciones diferentes.</p>
+
+                <p>El miedo nos hace temer por nuestra seguridad, integridad o incluso nuestra vida. Por otro lado, la ansiedad se puede producir ante una situación que nos parece amenazante para nuestros intereses. Por ejemplo, podemos sentir miedo cuando estamos a punto de ser inyectados o picados con una aguja, mientras que podemos sentir ansiedad cuando estamos a punto de exponer un tema en público.</p>
+
+                <h2>¿Qué actividades ayudan a disminuir la ansiedad?</h2>
+
+                <p>Aprender a controlar la ansiedad es un ejercicio constante. Puedes hacer distintas actividades físicas y psicológicas para apoyar los procesos de la ansiedad. Por ejemplo:</p>
+
+                <ul class="list_publicacion">
+                    <li><p>Hacer actidades deportivas que sean de agrado personal.</p></li>
+                    <li><p>Aprender y practicar <a onclick="goToSection('ejercicio_de_respiracion')" onkeydown="goToSection('ejercicio_de_respiracion')" class="same_page_link jovenes_link" tabindex="0">ejercicios de respiración</a>.</p></li>
+                    <li><p>Hacer <a onclick="goToSection('ejercicio_de_distraccion_mental')" onkeydown="goToSection('ejercicio_de_distraccion_mental')" class="same_page_link jovenes_link" tabindex="0">ejercicios de distracción mental</a>.</p></li>
+                </ul>
+
+                <h2 id="ejercicio_de_respiracion">Ejercicio de respiración</h2>
+
+                <p>La respiración profunda es una de las mejores maneras de relajar la mente, reducir el estrés o la ansiedad en el cuerpo, conciliar el sueño y disminuir las emociones intensas en caso de ser molestas en un momento inadecuado.</p>
+
+                <p>Una técnica de respiración conocida para relajarse es la técnica "4-7-8". Esta técnica consiste en inhalar aire por la nariz durante 4 segundos, aguantar el aire durante 7 segundos, exhalar el aire por la boca durante 8 segundos y repetir el proceso 3 veces más o hasta que se sienta menos ansiedad, estrés o se sienta más relajación.</p>
+
+                <h2 id="ejercicio_de_distraccion_mental">Ejercicio de distracción mental</h2>
+
+                <p>La distracción mental puede ser útil para dejar de pensar en un tema que esté afectado de manera negativa a nuestra mente y nos esté generando estrés, ansiedad o algún sentimiendo poco placentero.</p>
+
+                <p>Un ejercicio de distracción es:</p>
+
+                <ul class="list_publicacion">
+
+                    <li><p>Ver 5 cosas del mismo color</p></li>
+                    <li><p>Escuchar 4 cosas distintas</p></li>
+                    <li><p>Oler 3 cosas distintas</p></li>
+                    <li><p>Pensar en 2 cosas distintas que sean de agrado personal</p></li>
+                    <li><p>Respirar profundamente</p></li>
+
+                </ul>
+
+                <p>Se pueden cambiar los objetos de este ejercicio, por ejemplo, si no se pueden oler 3 cosas distintas, se puede pensar en 3 animales que sean de agrado personal.</p>
+
+			</span>
+
+            <div class="referencias jovenes_link">
+                <h3>Referencias: </h3>
+
+                <a target="_blank" href="https://www.mayoclinic.org/es-es/diseases-conditions/anxiety/symptoms-causes/syc-20350961">https://www.mayoclinic.org/es-es/diseases-conditions/anxiety/symptoms-causes/syc-20350961</a>
+                <a target="_blank" href="https://www.medicalnewstoday.com/articles/es/estres-y-ansiedad">https://www.medicalnewstoday.com/articles/es/estres-y-ansiedad</a>
+                <a target="_blank" href="https://www.cigna.com/es-us/knowledge-center/hw/temas-de-salud/manejo-del-estrs-uz2255">https://www.cigna.com/es-us/knowledge-center/hw/temas-de-salud/manejo-del-estrs-uz2255</a>
+            </div>
+
+			<h3 class="categorias_h3">Categoría</h3>
+			<div class="categorias_post">
+				<div class="categoria_post jovenes">Jóvenes</div>
+			</div>
+		</div>
+
+		
+		<div class="published">
+			<svg class="clock_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z"/></svg>
+			<p>Publicado el <span itemprop="datePublished" content="2022-11-13">13/11/2022</span></p>
+		</div>
+
+	</div>
+
+    <footer>
+        <div class="footer_content">
+            <p class="copyright footer_text"></p>
+            <a href="https://www.casa-arcoiris.org"><img class="footer_logo" src="https://www.casa-arcoiris.org/assets/casa_arcoiris_logo.png" alt="Logo de Casa Arcoíris"></a>
+        </div>
+    </footer>
+
+    <div id="close_element"></div>
+
+	<script src="https://www.casa-arcoiris.org/js/second.js"></script>
+    <script src="https://www.casa-arcoiris.org/js/dark-mode.js"></script>
+</body>
+</html>
